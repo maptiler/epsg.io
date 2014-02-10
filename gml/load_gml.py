024952d3faf0fad985bc5ba78bb0ed86186c602d
Tomas Pohanka <TomPohys@gmail.com>
2014-02-10 11:52:53 +0100

    Add examples of ids and urns in gml.sqlite

--- gml/load_gml.py
+++ gml/load_gml.py
@@ -4,6 +4,44 @@
 DATABASE = 'gml.sqlite'
 #XML = 'test_sample.xml'
 XML = 'GmlDictionary.xml'
+"""ids types in sqlite
+
+ogp-uom-1043
+epsg-uom-9001
+ogp-method-9834
+epsg-method-9835
+epsg-meridian-8901
+ogp-ellipsoid-7049
+epsg-ellipsoid-7050
+epsg-datum-9314
+ogp-datum-9315
+epsg-cs-6512
+epsg-axis-211
+ogp-cs-4493
+ogp-axis-30
+epsg-crs-7413
+ogp-crs-7414
+epsg-op-8635
+ogp-op-8636
+epsg-area-1024
+"""
+""" urn types in sqlite
+urn:ogc:def:change-request:EPSG::2014.001
+urn:ogc:def:version-history:EPSG::8.3.2
+urn:ogc:def:uom:EPSG::9101
+urn:ogc:def:method:EPSG::9629
+urn:ogc:def:meridian:EPSG::8910
+urn:ogc:def:ellipsoid:EPSG::7021
+urn:ogc:def:parameter:EPSG::8641
+urn:ogc:def:datum:EPSG::6600
+urn:ogc:def:cs:EPSG::4491
+urn:ogc:def:axis:EPSG::34
+urn:ogc:def:crs:EPSG::6329
+urn:ogc:def:coordinateOperation:EPSG::5984
+urn:ogc:def:area:EPSG::3760
+
+"""
+
 
 import sys
 import sqlite3 as sqlite
